2cd0d5c766356a3fd6643fc1c8db1c5eab13f04b
DonCri <36209368+DonCri@users.noreply.github.com>
2018-11-23 19:20:43 +0100

    Add files via upload

--- Alarmanlage/module.php
+++ Alarmanlage/module.php
@@ -161,7 +161,7 @@
           $array = json_decode($this->ReadPropertyString("Supplement"), true);
            
           
-          foreach ($array as $StatusID)
+          foreach ($array as $StatusID){}
           
           if(implode($StatusID) = true)
           {
