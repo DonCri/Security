797a176288fae568945df147391116053f4c9ee1
DonCri <36209368+DonCri@users.noreply.github.com>
2018-11-25 21:11:02 +0100

    Add files via upload

--- Alarmanlage/module.php
+++ Alarmanlage/module.php
@@ -277,7 +277,7 @@
                             if($Status == true)
                             {
                                 $arrName = array();
-                                $arrName[] = $InstanzName;
+                                $arrName[] = "$InstanzName";
                                 SetValue($this->GetIDforIdent("LastAlert"), $arrName);
                                 WFC_PushNotification($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text", "$AlertSound", $InstanzID);
                                 WFC_SendPopup($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text");
