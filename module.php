006a400cb85ff11d6fa79d9c7d8fce9e0ac9823e
DonCri <36209368+DonCri@users.noreply.github.com>
2019-04-03 22:35:14 +0200

    Add files via upload

--- module.php
+++ module.php
@@ -1,18 +1,21 @@
 <?
+		
     // Klassendefinition
     class Alarmanlage extends IPSModule {
-
+        
         // Der Konstruktor des Moduls
         // Überschreibt den Standard Kontruktor von IPS
         public function __construct($InstanceID) {
             // Diese Zeile nicht löschen
             parent::__construct($InstanceID);
 
+
             // Selbsterstellter Code
         }
-
+        
         // Überschreibt die interne IPS_Create($id) Funktion
-        public function Create() {
+	public function Create() {
+
             // Diese Zeile nicht löschen.
             parent::Create();
 
@@ -22,19 +25,18 @@
         			IPS_SetVariableProfileValues("BRELAG.AlarmModus", 0, 5, 0);
         			IPS_SetVariableProfileIcon("BRELAG.AlarmModus", "IPS");
         			IPS_SetVariableProfileAssociation("BRELAG.AlarmModus", 0, $this->Translate("ModeOne"), "", -1);
-        			IPS_SetVariableProfileAssociation("BRELAG.AlarmModus", 1, $this->Translate("ModeTwo"), "", -1);
-        			IPS_SetVariableProfileAssociation("BRELAG.AlarmModus", 2, $this->Translate("ModeOneTwo"), "", -1);
-        			IPS_SetVariableProfileAssociation("BRELAG.AlarmModus", 3, $this->Translate("ModeBell"), "", -1);
+        			IPS_SetVariableProfileAssociation("BRELAG.AlarmModus", 1, $this->Translate("ModeBell"), "", -1);
         		}
 
             //Progil für Quittierung
             if (!IPS_VariableProfileExists("BRELAG.Quittierung")) {
         			IPS_CreateVariableProfile("BRELAG.Quittierung", 1);
-        			IPS_SetVariableProfileValues("BRELAG.Quittierung", 0, 3, 0);
+        			IPS_SetVariableProfileValues("BRELAG.Quittierung", 0, 4, 0);
         			IPS_SetVariableProfileIcon("BRELAG.Quittierung", "IPS");
-        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 0, "Sabotage", "", -1);
-        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 1, "Batterie", "", -1);
-        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 2, "Lebensdauer", "", -1);
+        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 0, "Alarmmeldung", "", -1);
+        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 1, "Sabotage", "", -1);
+        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 2, "Batterie", "", -1);
+        			IPS_SetVariableProfileAssociation("BRELAG.Quittierung", 3, "Lebensdauer", "", -1);
         		}
 
             // Profil für Statusanzeige
@@ -44,12 +46,23 @@
         			IPS_SetVariableProfileAssociation("BRELAG.AlarmStatus", 0, $this->Translate("Off"), "", -1);
         			IPS_SetVariableProfileAssociation("BRELAG.AlarmStatus", 1, $this->Translate("On"), "", -1);
             }
+            
+            // Eigenschaften für Formular
+            $this->RegisterPropertyString("Supplement", "[]"); // Liste für boolean Variablen (z.B. Magnetkontakt -> Status). Können auch andere Variablen sein, solange es sich um Boolsche handelt.
+            $this->RegisterPropertyInteger("WebFrontName", 0); // Integer Wert für WebFront Auswahl. Wird für die Push-Nachrichten benötigt
+            $this->RegisterPropertyString("PushTitel", ""); // Titel welches in der Pusch-Nachricht angezeigt werden soll
+            $this->RegisterPropertyString("PushText", ""); // Test welches in der Pusch-Nachricht angezeigt werden soll
+            $this->RegisterPropertyString("AlertSound", ""); // Wählbare Alarm Sounds für Mobilgeräte (siehe Liste von Symcon
+            
             // Boolean für Statusanzeige der Alarmanlage, ist inaktiv!
             $this->RegisterVariableBoolean("State", "Status", "BRELAG.AlarmStatus", "0");
+            
+            // Zeigt der Letzte Alarm im Array (Zeigt nur der letzte Wert vom Array)
+            $this->RegisterVariableString("LastAlert", "Letzter Alarm", "", "0");
 
             // Stringvariable für Passwort Eingabe um Anlage scharf bzw. unschaf zu schalten, ist aktiv!
             $this->RegisterVariableString("Password", "Passwort Eingabe", "", "1");
-            $this->EnableAction("Password");
+            $this->EnableAction("Password");           
 
             // Integervariable für Auswahl der Modi, ist aktiv!
             $this->RegisterVariableInteger("Mode", "Modus", "BRELAG.AlarmModus", "2");
@@ -60,50 +73,103 @@
             $this->EnableAction("Quittierung");
 
             // Stringvariable für ändern des Passworts, Variable "Neues Passwort" verborgen aber beide aktiv!
-            $this->RegisterVariableString("OldPassword", "Aktuelles Passwort", "", "4");
+            $this->RegisterVariableString("OldPassword", "Passwort ändern (aktuelles Password eingeben)", "", "4");
             $this->EnableAction("OldPassword");
             $this->RegisterVariableString("NewPassword", "Neues Passwort", "", "5");
             $this->EnableAction("NewPassword");
             IPS_SetHidden($this->GetIDForIdent("NewPassword"), true);
 
-            // Eigenschaften für Formular
-            $this->RegisterPropertyString("Supplement", "[]");
-            $this->RegisterPropertyString("ID", "[]");
-
-
-            // Test Variable
-            $this->RegisterVariableString("TestString", "TestString", "", "0");
-            $this->EnableAction("TestString");
             
-            $this->RegisterVariableString("ContactName", "Magnetkontakt Name", "", "0");
-            
-            $this->RegisterVariableBoolean("TestBoolean", "TestBoolean", "", "0");
-            $this->EnableAction("TestBoolean");
+
+            // Test Variablen
+                      
         }
 
 
         // Überschreibt die intere IPS_ApplyChanges($id) Funktion
-        public function RequestAction($Ident, $Value) {
+	public function RequestAction($Ident, $Value) {
 
+
+            $AlarmState = GetValue($this->GetIDForIdent("State"));
+            $AlarmQuittierung = GetValue($this->GetIDForIdent("Quittierung"));
+            $Modus = GetValue($this->GetIDForIdent("Mode"));
+            
+            
               switch($Ident) {
                     case "Password":
                     //Neuen Wert in die Statusvariable schreiben
                       SetValue($this->GetIDForIdent($Ident), $Value);
                       $this->Activate();
                     break;
+                    
                     case "Mode":
-                      //Neuen Wert in die Statusvariable schreiben
-                      SetValue($this->GetIDForIdent($Ident), $Value);
+                      //Neuen Wert in die Statusvariable schreiben                      
+                      
+                            switch ($AlarmState)
+                            {
+                                case false:
+                                    switch($Modus)
+                                    {
+                                        case 0:
+                                            
+                                        break;
+                                        
+                                        case 1:
+                                            
+                                        break;
+                                    }
+                                    SetValue($this->GetIDForIdent($Ident), $Value);
+                                break;
+                                
+                                default:
+                                    echo "Alarm deaktivieren";
+                                break;
+                                
+                            }
+                            
+                      
                     break;
+                    
                     case "Quittierung":
                       //Neuen Wert in die Statusvariable schreiben
-                        SetValue($this->GetIDForIdent($Ident), $Value);
+                        
+                        switch ($AlarmState)
+                        {
+                            case false:
+                                switch ($AlarmQuittierung)
+                                {
+                                    case 0:
+                                        SetValue($this->GetIDForIdent("LastAlert"), "");
+                                    break;
+                                    
+                                    case 1:
+                                        
+                                    break;
+                                    
+                                    case 2:
+                                        
+                                    break;
+                                    
+                                    case 3:
+                                        
+                                    break;
+                                }
+                                SetValue($this->GetIDForIdent($Ident), $Value);
+                                // Platzhalter für Quittierfunktion
+                                break;
+                                
+                            default:
+                                echo "Alarm deaktivieren";
+                            break;
+                        }
                     break;
+                    
                     case "OldPassword":
                         //Neuen Wert in die Statusvariable schreiben
                         SetValue($this->GetIDForIdent($Ident), $Value);
                         $this->NewPassword();
                     break;
+                    
                     case "NewPassword":
                         //Neuen Wert in die Statusvariable schreiben
                         SetValue($this->GetIDForIdent($Ident), $Value);
@@ -111,19 +177,12 @@
                     }
 
       }
+      
+	public function Activate() {
 
-        /**
-        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
-        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
-        *
-        * ABC_MeineErsteEigeneFunktion($id);
-        *
-        */
-        public function Activate() {
             $Password = GetValue($this->GetIDForIdent("Password"));
-            $NewPassword = GetValue($this->GetIDForIdent("NewPassword"));
+            $currentPassword = GetValue($this->GetIDForIdent("NewPassword"));
             $State = GetValue($this->GetIDForIdent("State"));
-            $currentPassword = "";
 
 
             if($Password == $currentPassword && $State == false)
@@ -134,12 +193,17 @@
               {
                 SetValue($this->GetIDForIdent("State"), false);
                 SetValue($this->GetIDForIdent("Password"), "");
+              } elseif ($Password != $currentPassword)
+              {
+                  SetValue($this->GetIDForIdent("Password"), "");
+                  echo "Falsches Passwort, versuch es nochmals";
               }
               
         }
         
+        
+	public function NewPassword() {
 
-        public function NewPassword() {
 
           $Password = GetValue($this->GetIDForIdent("OldPassword"));
           $NewPassword = GetValue($this->GetIDForIdent("NewPassword"));
@@ -153,33 +217,95 @@
             IPS_Sleep(15000);
             IPS_SetHidden($this->GetIDForIdent("NewPassword"), true);
             
-          }
+          } else
+            {
+                SetValue($this->GetIDForIdent("OldPassword"), "");
+                echo "ACHTUNG: Falsches Passwort und / oder Anlage noch aktiv";                
+            }
           
 
         }
 
+       
         public function StateCheck() {
            
           $array = json_decode($this->ReadPropertyString("Supplement"), true);
-           
           
-          foreach ($array as $StatusID) 
+          $AlarmModus = GetValue($this->GetIDForIdent("Mode"));
+          $AlarmStatus = GetValue($this->GetIDForIdent("State"));
+          $Titel = $this->ReadPropertyString("PushTitel");
+          $Text = $this->ReadPropertyString("PushText");
+          $AlertSound = $this->ReadPropertyString("AlertSound");
+          
+          
+          switch($AlarmStatus)
           {
-                    $Status = GetValue($StatusID);
+              case true: // Alarm eingeschaltet
+                  
+                switch($AlarmModus)
+                    { 
+                    case 0: // Normaler Modus
+                   
+                        foreach ($array as $StatusID) 
+                            {
+                            $Status = GetValue($StatusID->ID);
+                            $InstanzID = IPS_GetParent($StatusID->ID);
+                            $InstanzName = IPS_GetName($InstanzID);        
                     
-                    if($Status == true)
-                    {
-                        echo "$Status Einbruch <br>";                        
-                        WFC_PushNotification(IPS_GetParent($Status), 'Warnung', 'Es regnet bald!', '', 0);
-                        SetValue($this->GetIDforIdent("ContactName"), IPS_GetParent($Status));
-                    }
+                            if($Status == true)
+                                {    
+                                    SetValue($this->GetIDforIdent("LastAlert"), $InstanzName);
+                                    
+                                    WFC_PushNotification($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text", "$AlertSound", $InstanzID);
+                                    WFC_SendPopup($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text");
+                                    
+                                }
+                               
+                            }
+                    break;
+                    
+                    case 1:
+                        foreach ($array as $StatusIDstring)
+                        {
+                            $StatusID = implode($StatusIDstring);
+                            $Status = GetValue($StatusID);
+                            $InstanzID = IPS_GetParent($StatusID);
+                            $InstanzName = IPS_GetName($InstanzID);
+                            
+                            if($Status == true)
+                            {
+                                
+                                SetValue($this->GetIDforIdent("LastAlert"), $arrName);
+                                WFC_PushNotification($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text", "$AlertSound", $InstanzID);
+                                WFC_SendPopup($this->ReadPropertyInteger("WebFrontName"), "$Titel", "$InstanzName $Text");
+                                
+                            }
+                            
+                        }
+                    break;
                   
-                  
+                    } 
+                    
+              break;  
           }
-          
-          
-          
-            SetValue($this->GetIDForIdent("TestBoolean"), GetValue(implode($array[0])) );
+   
+        }
+        
+        
+        public function AlarmQuittierung() {
+            
+        }
+        
+        public function ApplyChanges() {
+            
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+            
+            $StateUpdate = json_decode($this->ReadPropertyString("Supplement"));
+            foreach ($StateUpdate as $IDUpdate) {
+                $this->RegisterMessage($IDUpdate->ID, VM_UPDATE);
+            }
+            
             
         }
         
