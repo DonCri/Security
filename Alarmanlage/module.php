e5a8f808927edf1527c6eaa2f3b455d778c40e3f
DonCri <36209368+DonCri@users.noreply.github.com>
2019-04-04 00:51:56 +0200

    Add files via upload

--- Alarmanlage/module.php
+++ Alarmanlage/module.php
@@ -314,13 +314,8 @@
             
             $ID = json_decode($this->ReadPropertyString("Supplement"));
             foreach ($ID as $state) {
-                    $LastUpdate = IPS_GetVariable($state->ID);
-                    $TimeDif = strtotime("now") - $LastUpdate["VariableChanged"];
-                    
-                    if($TimeDif <= 60)
-                    {
                         $this->StateCheck();
-                    }
+                    
                     return;
             }
             
