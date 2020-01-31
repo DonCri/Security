2866b1862b7934c1577b4b7b6576151e6f5fed21
DonCri <36209368+DonCri@users.noreply.github.com>
2020-02-01 00:48:33 +0100

    Add files via upload

--- Alarmanlage/module.php
+++ Alarmanlage/module.php
@@ -433,7 +433,6 @@ class Alarmanlage extends IPSModule {
 			foreach ($StateUpdate4 as $IDUpdate4) {
                 $this->RegisterMessage($IDUpdate4->ID, VM_UPDATE);
 	    	}
-			AC_SetLoggingStatus($this->ReadPropertyInteger("ArchiveID"), $this->GetIDforIdent("LastAlert") , true);
 	}
         
 	public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
